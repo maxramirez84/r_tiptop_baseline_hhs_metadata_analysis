a25006d3dcdcd2358f9b841b9f506960f49cf377
maxramirez84 <maxramirez84@gmail.com>
2018-11-16 10:20:18 +0100

    Encapsulated code for building the calendar in a function.

--- redcap_logging_record_analysis.R
+++ redcap_logging_record_analysis.R
@@ -2,66 +2,64 @@ library(googleVis)
 
 nig_logging_file = "DATA/TIPTOPHHSBaselineNigeria_Logging_2018-11-15_1226.csv"
 mad_logging_file = "DATA/TIPTOPHHSBaselineMadagascarMAL_Logging_2018-11-15_1550.csv"
+drc_logging_file = "DATA/TIPTOPHHSBaselineDRC_Logging_2018-11-16_0850.csv"
+moz_logging_file = "DATA/TIPTOPHHSBaselineMozambique_Logging_2018-11-16_0913.csv"
 
 nig_data_manager = "aokoro"
 mad_data_manager = "rramananjato"
+drc_data_manager = "dndombe"
+moz_data_manager = "ejamisse"
 
-nig_logs = read.csv(nig_logging_file)
-mad_logs = read.csv(mad_logging_file)
-
-column_names = c("datetime", "user", "action", "description")
-colnames(nig_logs) = column_names
-colnames(mad_logs) = column_names
-
-nig_logs_from_field = subset(nig_logs, user == nig_data_manager)
-mad_logs_from_field = subset(mad_logs, user == mad_data_manager)
-
-nig_logs_from_field_tablets = nig_logs_from_field[grep("(API)", nig_logs_from_field$action), ]
-mad_logs_from_field_tablets = mad_logs_from_field[grep("(API)", mad_logs_from_field$action), ]
-
-nig_creation_logs = nig_logs_from_field_tablets[grep("Created Record", 
-                                                     nig_logs_from_field_tablets$action), ]
-mad_creation_logs = mad_logs_from_field_tablets[grep("Enregistrement créé", 
-                                                     mad_logs_from_field_tablets$action), ]
-
-nig_creation_logs$date = substr(nig_creation_logs$datetime, 0, 10)
-mad_creation_logs$date = substr(mad_creation_logs$datetime, 0, 10)
-
-nig_records_transferred_by_day = as.data.frame(table(nig_creation_logs$date))
-mad_records_transferred_by_day = as.data.frame(table(mad_creation_logs$date))
-
-column_names = c("date", "records")
-colnames(nig_records_transferred_by_day) = column_names
-colnames(mad_records_transferred_by_day) = column_names
-
-nig_records_transferred_by_day$date = as.Date(nig_records_transferred_by_day$date)
-mad_records_transferred_by_day$date = as.Date(mad_records_transferred_by_day$date)
-
-nig_calendar = gvisCalendar(
-  nig_records_transferred_by_day, 
-  datevar = "date", 
-  numvar = "records",
-  options = list(
-    title    = "Nigeria: Ohaukwu & Akure South",
-    calendar = "{cellSize: 25}",
-    noDataPattern = "{backgroundColor: '#ffffff'}",
-    width    = 1500,
-    height   = 320
+records_transferred_by_day_calendar = function(logging_file, lang = "EN", data_manager, title) {
+  api_mark = "(API)"
+  created_record_en_mark = "Created Record"
+  created_record_fr_mark = "Enregistrement créé"
+  
+  logs = read.csv(logging_file)
+  
+  colnames(logs) = c("datetime", "user", "action", "description")
+  
+  logs_from_field = subset(logs, user == data_manager)
+  logs_from_field_tablets = logs_from_field[grep(api_mark, logs_from_field$action), ]
+  
+  if(lang == "FR")
+    created_record_mark = created_record_fr_mark
+  else
+    created_record_mark = created_record_en_mark
+  
+  creation_logs = logs_from_field_tablets[grep(created_record_mark, 
+                                               logs_from_field_tablets$action), ]
+  creation_logs$date = substr(creation_logs$datetime, 0, 10)
+  
+  records_transferred_by_day = as.data.frame(table(creation_logs$date))
+  
+  colnames(records_transferred_by_day) = c("date", "records")
+  records_transferred_by_day$date = as.Date(records_transferred_by_day$date)
+  
+  calendar = gvisCalendar(
+    records_transferred_by_day, 
+    datevar = "date", 
+    numvar = "records",
+    options = list(
+      title    = title,
+      calendar = "{cellSize: 25}",
+      noDataPattern = "{backgroundColor: '#ffffff'}",
+      width    = 1500,
+      height   = 320
     )
   )
+  
+  return(calendar)
+}
 
-mad_calendar = gvisCalendar(
-  mad_records_transferred_by_day, 
-  datevar = "date", 
-  numvar = "records",
-  options = list(
-    title    = "Madagascar: Mananjary & Toliary II",
-    calendar = "{cellSize: 25}",
-    noDataPattern = "{backgroundColor: '#ffffff'}",
-    width    = 1500,
-    height   = 320
-    )
-  )
+nig_calendar = records_transferred_by_day_calendar(nig_logging_file, "EN", nig_data_manager,
+                                                   "NIG: Ohaukwu & Akure South")
+mad_calendar = records_transferred_by_day_calendar(mad_logging_file, "FR", mad_data_manager,
+                                                   "MAD: Mananjary & Toliary II")
+drc_calendar = records_transferred_by_day_calendar(drc_logging_file, "FR", drc_data_manager,
+                                                   "DRC: Kenge & Bulungu")
+moz_calendar = records_transferred_by_day_calendar(moz_logging_file, "EN", moz_data_manager,
+                                                   "MOZ: Nhamatanda & Meconta")
 
-calendar = gvisMerge(nig_calendar, mad_calendar)
+calendar = gvisMerge(gvisMerge(gvisMerge(nig_calendar, mad_calendar), drc_calendar), moz_calendar)
 plot(calendar)
