d034cb6fbf7d29a722b08c7004e4c1da3d81bbfb
maxramirez84 <maxramirez84@gmail.com>
2018-11-20 10:52:56 +0100

    Created a new calendar with records collected by day by using the interview_date field, in order to compare collection and transfers.

--- redcap_logging_record_analysis.R
+++ redcap_logging_record_analysis.R
@@ -11,13 +11,15 @@ drc_data_manager = "dndombe"
 moz_data_manager = "ejamisse"
 
 records_transferred_by_day_calendar = function(logging_file, lang = "EN", data_manager, title) {
+  #browser()
   api_mark = "(API)"
   created_record_en_mark = "Created Record"
   created_record_fr_mark = "Enregistrement créé"
+  interview_date_mark = "interview_date"
   
   logs = read.csv(logging_file)
   
-  colnames(logs) = c("datetime", "user", "action", "description")
+  colnames(logs) = c("log_datetime", "user", "action", "description")
   
   logs_from_field = subset(logs, user == data_manager)
   logs_from_field_tablets = logs_from_field[grep(api_mark, logs_from_field$action), ]
@@ -29,19 +31,26 @@ records_transferred_by_day_calendar = function(logging_file, lang = "EN", data_m
   
   creation_logs = logs_from_field_tablets[grep(created_record_mark, 
                                                logs_from_field_tablets$action), ]
-  creation_logs$date = substr(creation_logs$datetime, 0, 10)
   
-  records_transferred_by_day = as.data.frame(table(creation_logs$date))
+  # Remove logs of records without interview_date
+  creation_logs = creation_logs[regexpr(interview_date_mark, creation_logs$description) >= 0, ]
   
-  colnames(records_transferred_by_day) = c("date", "records")
-  records_transferred_by_day$date = as.Date(records_transferred_by_day$date)
+  creation_logs$transfer_date = substr(creation_logs$log_datetime, 0, 10)
+  creation_logs$record_date = substr(creation_logs$description, 
+                                     regexpr(interview_date_mark, creation_logs$description) + 18, 
+                                     regexpr(interview_date_mark, creation_logs$description) + 18 + 9)
   
-  calendar = gvisCalendar(
+  records_transferred_by_day = as.data.frame(table(creation_logs$transfer_date))
+  
+  colnames(records_transferred_by_day) = c("transfer_date", "records")
+  records_transferred_by_day$transfer_date = as.Date(records_transferred_by_day$transfer_date)
+  
+  transfers_calendar = gvisCalendar(
     records_transferred_by_day, 
-    datevar = "date", 
+    datevar = "transfer_date", 
     numvar = "records",
     options = list(
-      title    = paste0(title, " (", nrow(creation_logs), " records sent)"),
+      title    = paste0("TRANSFERS: ", title, " (", nrow(creation_logs), " records sent)"),
       titleTextStyle = "{color: 'red', fontSize: 12}",
       calendar = "{cellSize: 25}",
       # noDataPattern = "{backgroundCasxolor: '#ffffff'}",
@@ -51,7 +60,27 @@ records_transferred_by_day_calendar = function(logging_file, lang = "EN", data_m
     )
   )
   
-  return(calendar)
+  records_collected_by_day = as.data.frame(table(creation_logs$record_date))
+  
+  colnames(records_collected_by_day) = c("record_date", "records")
+  records_collected_by_day$record_date = as.Date(records_collected_by_day$record_date)
+  
+  collected_calendar = gvisCalendar(
+    records_collected_by_day, 
+    datevar = "record_date", 
+    numvar = "records",
+    options = list(
+      title    = paste0("COLLECTED: ", title, " (", nrow(creation_logs), " records collected)"),
+      titleTextStyle = "{color: 'red', fontSize: 12}",
+      calendar = "{cellSize: 25}",
+      # noDataPattern = "{backgroundCasxolor: '#ffffff'}",
+      colorAxis = "{colors:['#ECF2FF','#17357A']}",
+      width    = 1500,
+      height   = 320
+    )
+  )
+  
+  return(gvisMerge(collected_calendar, transfers_calendar))
 }
 
 nig_calendar = records_transferred_by_day_calendar(nig_logging_file, "EN", nig_data_manager,
